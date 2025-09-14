97958faff23c6aa66806b4bc918125ce446e9c39
Delilah Clarke <dylanjclarke18@gmail.com>
2025-09-14 17:10:31 -0400

    Added a test to config_parse.rs

--- src/config_parse.rs
+++ src/config_parse.rs
@@ -1,20 +1,23 @@
+use std::path::Path;
+
 use serde::Deserialize;
-use toml::from_str;
 
-
-#[derive(Deserialize)]
+#[derive(Debug, Default, Deserialize, PartialEq)]
 pub struct Config {
+    #[serde(default)]
     pub runner: Runner,
+    #[serde(default)]
     pub scheduler: Scheduler,
+    #[serde(default)]
     pub cost: Cost,
+    #[serde(default)]
     pub logging: Logging,
-    #[serde(rename = "rule")]
+    #[serde(default, rename = "rule")]
     pub rules: Vec<Rule>,
-
 }
 
-#[derive(Default, Deserialize)]
-pub struct Rule{
+#[derive(Debug, Default, Deserialize, PartialEq)]
+pub struct Rule {
     pub name: String,
     pub lhs: String,
     pub rhs: String,
@@ -28,34 +31,80 @@ pub struct Rule{
     pub conditions: Vec<String>,
 }
 
-#[derive(Deserialize)]
-pub struct Runner{
+#[derive(Debug, Deserialize, PartialEq)]
+pub struct Runner {
     pub iteration_limit: u32,
     pub node_limit: u32,
     pub timeout_ms: u32,
 }
 
-#[derive(Deserialize)]
-pub struct Scheduler{
+impl Default for Runner {
+    fn default() -> Self {
+        Runner {
+            iteration_limit: 30,
+            node_limit: 10_000,
+            timeout_ms: 5000,
+        }
+    }
+}
+
+#[derive(Debug, Default, Deserialize, PartialEq)]
+pub struct Scheduler {
     pub disabled: bool,
     pub ban_length: u32,
     pub match_limit: u32,
 }
 
-#[derive(Deserialize)]
-pub struct Cost{
+#[derive(Debug, Default, Deserialize, PartialEq)]
+pub struct Cost {
     pub operator_weight: u32,
     pub number_weight: f32,
     pub variable_weight: f32,
 }
 
-#[derive(Deserialize)]
-pub struct Logging{
+#[derive(Debug, Default, Deserialize, PartialEq)]
+pub struct Logging {
     pub disabled: bool,
     pub trace_rules: Vec<String>,
 }
 
-pub fn load_config(){
-    let source = std::fs::read_to_string("config.toml").unwrap();
-    let config: Config = toml::from_str(&source).unwrap();
+pub fn load_config(path: &Path) -> Config {
+    std::fs::read_to_string(path)
+        .and_then(|source| toml::from_str(&source)
+            .map_err(|err| std::io::Error::other(err)))
+        .unwrap_or_default()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_empty_config() {
+        let config: Config = toml::from_str("").unwrap();
+        assert_eq!(
+            config,
+            Config {
+                runner: Runner {
+                    iteration_limit: 0,
+                    node_limit: 0,
+                    timeout_ms: 0,
+                },
+                scheduler: Scheduler {
+                    disabled: false,
+                    ban_length: 0,
+                    match_limit: 0,
+                },
+                cost: Cost {
+                    operator_weight: 0,
+                    number_weight: 0.0,
+                    variable_weight: 0.0,
+                },
+                logging: Logging {
+                    disabled: false,
+                    trace_rules: Vec::new(),
+                },
+                rules: Vec::new(),
+            }
+        )
+    }
 }
